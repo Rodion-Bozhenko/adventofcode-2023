d775fbcc9638eae319aef26c98894439c90eba30
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-12-04 15:00:26 +0100

    Rust | Day 1 - Part 2

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{prelude::*, BufReader, Result};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let file = File::open("../../input.txt")?;
     let reader = BufReader::new(file);
 
@@ -26,5 +27,72 @@ fn main() -> io::Result<()> {
     }
     println!("Part 1 SUM: {}", sum);
 
+    let _ = part_two();
     Ok(())
+}
+
+fn part_two() -> Result<()> {
+    let file = File::open("../../input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut sum: i32 = 0;
+    for line in reader.lines() {
+        let line = line?;
+        let mut left: String = "".to_owned();
+        let mut right: String = "".to_owned();
+
+        for (i, c) in line.chars().enumerate() {
+            if let Some(digit) = lookup_word(line.clone().get(..i).unwrap().to_owned()) {
+                if left.is_empty() {
+                    left = digit.to_string();
+                }
+            }
+            if c.is_ascii_digit() && left.is_empty() {
+                left = c.to_string();
+            }
+            if !left.is_empty() {
+                sum += left.parse::<i32>().unwrap() * 10;
+                break;
+            }
+        }
+        let mut i = line.chars().count();
+        for (_, c) in line.chars().rev().enumerate() {
+            if c.is_ascii_digit() {
+                right = c.to_string();
+            }
+            if let Some(digit) = lookup_word(line.clone().get(i..).unwrap().to_owned()) {
+                right = digit.to_string();
+            }
+            if !right.is_empty() {
+                sum += right.parse::<i32>().unwrap();
+                break;
+            }
+            i -= 1;
+        }
+    }
+    println!("Part 2 SUM: {}", sum);
+
+    Ok(())
+}
+
+fn lookup_word(line: String) -> Option<i32> {
+    let lookup_table = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    for (k, _) in lookup_table.iter() {
+        if line.contains(k) {
+            return Some(lookup_table[k]);
+        }
+    }
+
+    return None;
 }
