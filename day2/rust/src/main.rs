53a1ab79f81266d98be309d42d01247e8fbe5851
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-12-05 21:35:11 +0100

    Rust | Day2 - Part 2

--- day2/rust/src/main.rs
+++ day2/rust/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
 
 fn main() -> Result<()> {
+    let _ = part_two();
     part_one()
 }
 
@@ -19,11 +20,9 @@ fn part_one() -> Result<()> {
         .enumerate()
         .filter_map(|(n, line)| {
             let line = line.unwrap();
-            println!("LINE: {}", line);
             let mut iter = line.split_ascii_whitespace().skip(2);
             while let Some(x) = iter.next() {
                 let count: u32 = x.parse().unwrap();
-                println!("COUNT: {}", count);
                 let color = iter.next().unwrap().trim_end_matches([',', ';']);
                 if count > colors[color] {
                     return None;
@@ -34,6 +33,39 @@ fn part_one() -> Result<()> {
         .sum::<usize>();
 
     println!("PART 1 RESULT: {}", result);
+
+    Ok(())
+}
+
+fn part_two() -> Result<()> {
+    let input_file = File::open("../../input.txt")?;
+    let reader = BufReader::new(input_file);
+
+    let result: u32 = reader
+        .lines()
+        .map(|line| {
+            let mut colors: HashMap<String, u32> = [
+                (String::from("red"), 0),
+                (String::from("green"), 0),
+                (String::from("blue"), 0),
+            ]
+            .into_iter()
+            .collect();
+            let line: &str = &line.unwrap();
+            let mut iter = line.split_ascii_whitespace().skip(2);
+            while let Some(x) = iter.next() {
+                let count: u32 = x.parse().unwrap();
+                let color = iter.next().unwrap().trim_end_matches([',', ';']);
+                let max = colors.get_mut(color).unwrap();
+                if count > *max {
+                    *max = count;
+                }
+            }
+            colors.into_iter().map(|(_, count)| count).product::<u32>()
+        })
+        .sum();
+
+    println!("PART 2 RESULT: {}", result);
 
     Ok(())
 }
